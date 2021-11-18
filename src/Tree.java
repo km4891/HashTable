b0c2ac4026c5711fbae79c0ecfbad36fb2235fa4
km4891 <kmorarity@gmail.com>
2021-11-17 21:44:59 -0800

    tree started

--- src/Tree.java
+++ src/Tree.java
@@ -54,4 +54,88 @@ public class Tree {
 		} 
 	} 
 
+    public boolean delete(int key) // delete node with given key
+	{ // (assumes non-empty list)
+		Node current = root;
+		Node parent = root;
+		boolean isLeftChild = true;
+		while(current.data != key) // search for node
+		{
+			parent = current;
+			if(key < current.data) // go left?
+			{
+				isLeftChild = true;
+				current = current.leftChild;
+			}
+			else // or go right?
+			{
+				isLeftChild = false;
+				current = current.rightChild;
+			}
+			if(current == null) // end of the line,
+				return false; // didn't find it
+		} // end while
+		// found node to delete
+		// if no children, simply delete it
+		if(current.leftChild==null &&
+				current.rightChild==null) {
+			if(current == root) // if root,
+				root = null; // tree is empty
+			else if(isLeftChild)
+				parent.leftChild = null; // disconnect
+			else // from parent
+				parent.rightChild = null;
+		}
+		// if no right child, replace with left subtree
+		else if(current.rightChild==null)
+			if(current == root)
+				root = current.leftChild;
+			else if(isLeftChild)
+				parent.leftChild = current.leftChild;
+			else
+				parent.rightChild = current.leftChild;
+		// if no left child, replace with right subtree
+		else if(current.leftChild==null)
+			if(current == root)
+				root = current.rightChild;
+			else if(isLeftChild)
+				parent.leftChild = current.rightChild;
+			else
+				parent.rightChild = current.rightChild;
+		else // two children, so replace with inorder successor
+		{
+			// get successor of node to delete (current)
+			Node successor = getSuccessor(current);
+			// connect parent of current to successor instead
+			if(current == root)
+				root = successor;
+			else if(isLeftChild)
+				parent.leftChild = successor;
+			else
+				parent.rightChild = successor;
+			// connect successor to current's left child
+			successor.leftChild = current.leftChild;
+		} // end else two children
+		// (successor cannot have a left child)
+		return true; // success
+	} // end delete()
+
+    private Node getSuccessor(Node delNode) {
+		Node successorParent = delNode;
+		Node successor = delNode;
+		Node current = delNode.rightChild; // go to right child
+		while(current != null) // until no more
+		{ // left children,
+			successorParent = successor;
+			successor = current;
+			current = current.leftChild; // go to left child
+		}
+		// if successor not
+		if(successor != delNode.rightChild) // right child,
+		{ // make connections
+			successorParent.leftChild = successor.rightChild;
+			successor.rightChild = delNode.rightChild;
+		}
+		return successor;
+	}
 }
